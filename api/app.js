2f8bf6c484c1ce7b4040117f1bd45815e088f65f
juan <juancraft12yt@gmail.com>
2021-04-27 23:05:38 -0300

    Create app.js

---
+++ api/app.js
@@ -0,0 +1,23 @@
+require("dotenv").config({ path: "../.env" });
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("working!");
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+const PORT = 8080;
+app.listen(process.env.PORT || PORT, console.log(`Listening at port ${PORT}`));
