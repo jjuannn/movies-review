8a6c381a33481aa96201c4e13ae89f299275aa52
juan <juancraft12yt@gmail.com>
2021-04-29 00:04:39 -0300

    Create getReviews function

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,28 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8080/reviews/create";
+const BASE_URL = "http://localhost:8080/reviews";
 
-export function createReview(post) {
+export function createReview(review) {
   return axios({
     method: "POST",
-    url: BASE_URL,
+    url: `${BASE_URL}/create`,
     headers: { "Content-Type": "application/json" },
-    data: post,
+    data: review,
   })
     .then((res) => {
       return res;
+    })
+    .catch((err) => {
+      throw new Error(err.response.data);
+    });
+}
+
+export function getReviews() {
+  return axios({
+    method: "GET",
+    url: `${BASE_URL}/all`,
+  })
+    .then((res) => {
+      return res.data.results;
     })
     .catch((err) => {
       throw new Error(err.response.data);
